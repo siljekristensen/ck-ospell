c3603e68ca9bb78e7e07269559ce8f39bbdaa659
Pascal Hertleif <killercup@gmail.com>
2016-07-06 17:01:20 +0200

    Format Localisation Code

--- public/local/de/local.js
+++ public/local/de/local.js
@@ -1,70 +1,45 @@
-/**
- * Giella v3.1.8436
- * Copyright (c) 1999-2016 SpellChecker.net, Inc. All rights reserved.
- */
-/*!
- * jQuip JavaScript Library v0.0.3
- * https://github.com/mythz/jquip
- *
- * Copyright, Demis Bellot
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- *
- */
-/*!
- * Rangy, a cross-browser JavaScript range and selection library
- * http://code.google.com/p/rangy/
- *
- * Copyright 2012, Tim Down
- * Licensed under the MIT license.
- * Version: 1.3alpha.681
- * Build date: 20 July 2012
- */
-/*! @license
- * JS Signals <http://millermedeiros.github.com/js-signals/>
- * Released under the MIT license
- * Author: Miller Medeiros
- * Version: 0.8.1 - Build: 266 (2012/07/31 03:33 PM)
- */
-! function(a, b) {
-    a && (a.prototype.Localization.lang[b] = {
-        btn_about: "Über Giella",
-        btn_addWord: "Wort hinzufügen",
-        btn_createDic: "Erzeugen",
-        btn_deleteDic: "Löschen",
-        btn_disable: "Giella ausschalten",
-        btn_enable: "Giella einschalten",
-        btn_ignore: "Ignorieren",
-        btn_ignoreAll: "Alle ignorieren",
-        btn_langs: "Sprachen",
-        btn_moreSuggestions: "Mehr Vorschläge",
-        btn_noSuggestions: "No suggestion",
-        btn_renameDic: "Umbenennen",
-        btn_restoreDic: "Wiederherstellen",
-        btn_toggle: "Giella umschalten",
-        btn_options: "Optionen",
-        label_allCaps: "Groß geschriebenen Wörter ignorieren",
-        label_fieldNameDic: "Wörterbuchname",
-        label_ignoreDomainNames: "Domain-Namen ignorieren",
-        label_mixedCase: "Wörter mit gemischte Setzkasten ignorieren",
-        label_mixedWithDigits: "Wörter mit Zahlen ignorieren",
-        message_info_emptyDic: "Wörterbuchname sollte leer sein.",
-        message_info_opera: "Nicht von Opera unterstützt",
-        tab_about: "Über",
-        tab_dictionaries: "Wörterbücher",
-        tab_languages: "Sprachen",
-        tab_options: "Optionen",
-        text_descriptionDic: "Anfangs wird das Benutzerwörterbuch in einem Cookie gespeichert. Allerdings sind Cookies in der Größe begrenzt. Wenn das Benutzerwörterbuch bis zu einem Punkt wächst, wo es nicht mehr in einem Cookie gespeichert werden kann, wird das Benutzerwörterbuch auf dem Server gespeichert. Um Ihr persönliches Wörterbuch auf dem Server zu speichern, müssen Sie einen Namen für das Wörterbuch angeben. Falls  Sie schon ein gespeicherte Wörterbuch haben, geben Sie bitte dessen Namen ein und klicken Sie auf die Schaltfläche Wiederherstellen.",
-        text_title: "Rechtschreibprüfung während der Texteingabe (Giella)",
-        text_copyrights: "&copy; <a style='text-decoration: underline;cursor:pointer;color:#00f' href='http://www.webspellchecker.net/' target='_blank'>1999-2016 SpellChecker.net, Inc.</a><br />All Rights Reserved.",
-        version: "Version: ",
-        message_success_createDic: "Wörterbuch %s wurde erstellt.",
-        message_error_createDic: "Wörterbuch %s kann nicht erstellt werden.",
-        message_success_removeDic: "Wörterbuch %s wurde entfernt.",
-        message_error_removeDic: "Wörterbuch %s kann nicht umbenannt werden.",
-        message_success_restoreDic: "Wörterbuch %s wurde wiederhergestellt.",
-        message_error_restoreDic: "Wörterbuch %s kann nicht wiederhergestellt werden.",
-        message_success_renameDic: "Wörterbuch %s erfolgreich umbenannt.",
-        message_error_renameDic: "Wörterbuch %s kann nicht umbenannt werden."
-    })
+!function(a, b) {
+  a && (a.prototype.Localization.lang[b] = {
+    btn_about : "Über Giella",
+    btn_addWord : "Wort hinzufügen",
+    btn_createDic : "Erzeugen",
+    btn_deleteDic : "Löschen",
+    btn_disable : "Giella ausschalten",
+    btn_enable : "Giella einschalten",
+    btn_ignore : "Ignorieren",
+    btn_ignoreAll : "Alle ignorieren",
+    btn_langs : "Sprachen",
+    btn_moreSuggestions : "Mehr Vorschläge",
+    btn_noSuggestions : "No suggestion",
+    btn_renameDic : "Umbenennen",
+    btn_restoreDic : "Wiederherstellen",
+    btn_toggle : "Giella umschalten",
+    btn_options : "Optionen",
+    label_allCaps : "Groß geschriebenen Wörter ignorieren",
+    label_fieldNameDic : "Wörterbuchname",
+    label_ignoreDomainNames : "Domain-Namen ignorieren",
+    label_mixedCase : "Wörter mit gemischte Setzkasten ignorieren",
+    label_mixedWithDigits : "Wörter mit Zahlen ignorieren",
+    message_info_emptyDic : "Wörterbuchname sollte leer sein.",
+    message_info_opera : "Nicht von Opera unterstützt",
+    tab_about : "Über",
+    tab_dictionaries : "Wörterbücher",
+    tab_languages : "Sprachen",
+    tab_options : "Optionen",
+    text_descriptionDic :
+        "Anfangs wird das Benutzerwörterbuch in einem Cookie gespeichert. Allerdings sind Cookies in der Größe begrenzt. Wenn das Benutzerwörterbuch bis zu einem Punkt wächst, wo es nicht mehr in einem Cookie gespeichert werden kann, wird das Benutzerwörterbuch auf dem Server gespeichert. Um Ihr persönliches Wörterbuch auf dem Server zu speichern, müssen Sie einen Namen für das Wörterbuch angeben. Falls  Sie schon ein gespeicherte Wörterbuch haben, geben Sie bitte dessen Namen ein und klicken Sie auf die Schaltfläche Wiederherstellen.",
+    text_title : "Rechtschreibprüfung während der Texteingabe (Giella)",
+    text_copyrights :
+        "&copy; <a style='text-decoration: underline;cursor:pointer;color:#00f' href='http://www.webspellchecker.net/' target='_blank'>1999-2016 SpellChecker.net, Inc.</a><br />All Rights Reserved.",
+    version : "Version: ",
+    message_success_createDic : "Wörterbuch %s wurde erstellt.",
+    message_error_createDic : "Wörterbuch %s kann nicht erstellt werden.",
+    message_success_removeDic : "Wörterbuch %s wurde entfernt.",
+    message_error_removeDic : "Wörterbuch %s kann nicht umbenannt werden.",
+    message_success_restoreDic : "Wörterbuch %s wurde wiederhergestellt.",
+    message_error_restoreDic :
+        "Wörterbuch %s kann nicht wiederhergestellt werden.",
+    message_success_renameDic : "Wörterbuch %s erfolgreich umbenannt.",
+    message_error_renameDic : "Wörterbuch %s kann nicht umbenannt werden."
+  })
 }(window.GIELLA, "de");
